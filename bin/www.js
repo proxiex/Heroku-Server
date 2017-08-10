fc21c4dad0f336ca53ec95f4160fd6ebb02a7997
samuel <difrentecho@gmail.com>
2017-08-10 19:06:31 +0100

    rectified server issues

--- bin/www.js
+++ bin/www.js
@@ -1,8 +1,8 @@
 // This will be our application entry. We'll setup our server here.
 require('babel-register')
+require('dotenv').config()
 const http = require('http')
 const app = require('../app') // The express app we just created
-require('dotenv').config()
 
 const port = parseInt(process.env.PORT, 10) || 8000
 app.set('port', port)
