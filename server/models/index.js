6cfdaf0aac4a5bcee1ee66fad929516ef7b34fc9
samuel <difrentecho@gmail.com>
2017-08-10 19:23:33 +0100

    rectified server issues

--- server/models/index.js
+++ server/models/index.js
@@ -8,8 +8,9 @@ const config = require('../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_varialbe) {
-    sequelize = new Sequelize(process.env[config.use_env_varialbe]);
+console.log(config, env);
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
     sequelize = new Sequelize(
         config.database, config.username, config.password, config
